b8d05c92ea11be8ee5b5920ddce149fa06699cff
Ava Lehner <avalehner@gmail.com>
2026-04-07 12:29:40 -0400

    Fill out frequency map for each key code

--- index.js
+++ index.js
@@ -2,8 +2,41 @@ console.log("Hello!");
 
 const keys = {
   KeyA: {
-    frequency: 262
+    frequency: 261.63,
   },
+  KeyW: {
+    frequency: 277.18,
+  },
+  KeyS: {
+    frequency: 296.66,
+  },
+  KeyE: {
+    frequency: 311.13
+  },
+  KeyD: {
+    frequency: 329.63,
+  },
+  KeyF: {
+    frequency: 349.23,
+  },
+  KeyT: {
+    frequency: 369.99,
+  },
+  KeyG: {
+    frequency: 392.00,
+  },
+  KeyY: {
+    frequency: 415.30,
+  },
+  KeyH: {
+    frequency: 440.00,
+  },
+  KeyU: {
+    frequency: 466.16,
+  },
+  KeyJ: {
+    frequency: 493.88,
+  }
 }
 
 window.addEventListener("keydown", (event) => {
